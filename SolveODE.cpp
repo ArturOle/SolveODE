b9069d9dba33a947232d6996c0385778b637550d
ArturOle <artur.oleksinski99@gmail.com>
2021-06-21 13:00:12 +0200

    Naprawione

--- SolveODE.cpp
+++ SolveODE.cpp
@@ -3,7 +3,8 @@
 ODE::ODE(std::vector<double> parameters_in, double h_in)
 {
     this->parameters = parameters_in;
-    this->table[0] = {0, parameters[3], 0, 0};
+    this->table[0].reserve(5);
+    this->table.push_back({0,parameters[3],0,0,0});
     this->h = h_in;
     function(0);
 }
@@ -22,6 +23,6 @@ std::vector<double> ODE::solve_euler()
 double ODE::function(int i)
 {
 
-    std::cout<< parameters[0]*parameters[3] << std::endl ;//+ parameters[1]*table[i][0] + parameters[2]*table[i][0]*table[i][0];
+    std::cout<< table[i][1] << std::endl ;//+ parameters[1]*table[i][0] + parameters[2]*table[i][0]*table[i][0];
     return parameters[3];
 }
--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ int main()
 {
     std::vector<double> parameters = {-5.8, 4.7, -0.9, 1.9, 9};
 
-    ODE equation(parameters, 0.2);
-    std::cout<<equation.function(0)<<std::endl;
+    ODE *equation = new ODE(parameters, 0.2);
+    std::cout<< equation->function(0)<<std::endl;
+    delete equation;
 
 }
